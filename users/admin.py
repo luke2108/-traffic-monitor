2e26570729fb53b041b3a6e78caa5db7793a512d
Luke <Luke@JZ28B-263.JZ28B-263>
2023-12-11 17:58:43 +0800

    first commit

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserError
+
+
+class UserMonitorAdmin(admin.ModelAdmin):
+    list_display = ["ip_user", "user_agent", "domain", "status", "timestamp"]
+    search_fields = ["name"]
+
+admin.site.register(UserError, UserMonitorAdmin)
