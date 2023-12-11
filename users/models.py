29041e4d86f9abbbe603e943371573359a3099d7
Luke <Luke@JZ28B-263.JZ28B-263>
2023-12-11 17:42:17 +0800

    add traffic monitor

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class UserError(models.Model):
     id = models.AutoField(primary_key=True)
     ip_user = models.CharField(max_length=100, null=True, blank=True)
-    user_a = models.CharField(max_length=500, null=True, blank=True)
     user_agent = models.CharField(max_length=10000, null=True, blank=True)
     domain = models.CharField(max_length=500, null=True, blank=True)
     status = models.CharField(max_length=50, null=True, blank=True)
