0b951dbbde2fe64f03be32581e30fad001a81ff3
Luke <Luke@JZ28B-263.JZ28B-263>
2023-12-12 08:45:47 +0800

    add

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,6 @@ from .models import UserError
 
 class UserMonitorAdmin(admin.ModelAdmin):
     list_display = ["ip_user", "user_agent", "domain", "status", "timestamp"]
-    search_fields = ["name"]
+    search_fields = ["ip_user"]
 
 admin.site.register(UserError, UserMonitorAdmin)
